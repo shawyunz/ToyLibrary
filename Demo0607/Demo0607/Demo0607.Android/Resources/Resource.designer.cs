6d57d20f0bdfffd583ae91a32bc10768306821a5
Shaw6157 <43285754+Shaw6157@users.noreply.github.com>
2019-07-14 23:20:38 +1200

    [listing] waterfall test version
    todo:
    left grid margin
    make a method for adding the card

--- Demo0607/Demo0607/Demo0607.Android/Resources/Resource.designer.cs
+++ Demo0607/Demo0607/Demo0607.Android/Resources/Resource.designer.cs
@@ -4345,26 +4345,26 @@ namespace Demo0607.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020133
-			public const int avd_hide_password_1 = 2130837811;
-			
-			// aapt resource value: 0x7f020134
-			public const int avd_hide_password_2 = 2130837812;
-			
 			// aapt resource value: 0x7f020135
-			public const int avd_hide_password_3 = 2130837813;
+			public const int avd_hide_password_1 = 2130837813;
+			
+			// aapt resource value: 0x7f020136
+			public const int avd_hide_password_2 = 2130837814;
+			
+			// aapt resource value: 0x7f020137
+			public const int avd_hide_password_3 = 2130837815;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020136
-			public const int avd_show_password_1 = 2130837814;
-			
-			// aapt resource value: 0x7f020137
-			public const int avd_show_password_2 = 2130837815;
-			
 			// aapt resource value: 0x7f020138
-			public const int avd_show_password_3 = 2130837816;
+			public const int avd_show_password_1 = 2130837816;
+			
+			// aapt resource value: 0x7f020139
+			public const int avd_show_password_2 = 2130837817;
+			
+			// aapt resource value: 0x7f02013a
+			public const int avd_show_password_3 = 2130837818;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -5002,11 +5002,11 @@ namespace Demo0607.Droid
 			// aapt resource value: 0x7f020128
 			public const int notification_icon_background = 2130837800;
 			
-			// aapt resource value: 0x7f020131
-			public const int notification_template_icon_bg = 2130837809;
+			// aapt resource value: 0x7f020133
+			public const int notification_template_icon_bg = 2130837811;
 			
-			// aapt resource value: 0x7f020132
-			public const int notification_template_icon_low_bg = 2130837810;
+			// aapt resource value: 0x7f020134
+			public const int notification_template_icon_low_bg = 2130837812;
 			
 			// aapt resource value: 0x7f020129
 			public const int notification_tile_bg = 2130837801;
@@ -5030,7 +5030,13 @@ namespace Demo0607.Droid
 			public const int tooltip_frame_light = 2130837807;
 			
 			// aapt resource value: 0x7f020130
-			public const int xamarin_logo = 2130837808;
+			public const int triangle = 2130837808;
+			
+			// aapt resource value: 0x7f020131
+			public const int triangle2 = 2130837809;
+			
+			// aapt resource value: 0x7f020132
+			public const int xamarin_logo = 2130837810;
 			
 			static Drawable()
 			{
--- Demo0607/Demo0607/Demo0607/Views/ItemsPage.xaml.cs
+++ Demo0607/Demo0607/Demo0607/Views/ItemsPage.xaml.cs
@@ -23,6 +23,83 @@ namespace Demo0607.Views
             InitializeComponent();
 
             BindingContext = viewModel = new ItemsViewModel();
+
+            double _width = 160;
+            double _smallheight = 60;
+            double _bigheight = 130;
+
+
+            var grid0 = new Grid { ColumnSpacing = 0, HorizontalOptions = LayoutOptions.FillAndExpand };
+            grid0.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            grid0.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+
+            var grid1 = new Grid { HorizontalOptions = LayoutOptions.CenterAndExpand };
+            grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(_bigheight) });
+            grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(_smallheight) });
+            grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(_bigheight) });
+            grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(_smallheight) });
+
+
+            var image1 = new Image { Source = "sp01.jpg" };
+            var entry1 = new Label { Text = "test0001" };
+            grid1.Children.Add(image1, 0, 0);
+            grid1.Children.Add(entry1, 0, 1);
+
+            //AddItemLayout(grid1, "sp01.jpg", "test0001", "", "");
+
+
+            var image3 = new Image { Source = "sp03.jpg" };
+            var entry3 = new Label { Text = "test0003" };
+            grid1.Children.Add(image3, 0, 2);
+            grid1.Children.Add(entry3, 0, 3);
+
+
+            var image99 = new Image { Source = "triangle2.png", HorizontalOptions = LayoutOptions.Start, VerticalOptions = LayoutOptions.End, Margin = new Thickness(30, 0, 0, 0), WidthRequest = 24, HeightRequest = 12 };
+            grid1.Children.Add(image99, 0, 0);
+            var image88 = new Image { Source = "triangle2.png", HorizontalOptions = LayoutOptions.Start, VerticalOptions = LayoutOptions.End, Margin = new Thickness(30, 0, 0, 0), WidthRequest = 24, HeightRequest = 12 };
+            grid1.Children.Add(image88, 0, 2);
+
+
+            var grid2 = new Grid { HorizontalOptions = LayoutOptions.CenterAndExpand };
+            grid2.RowDefinitions.Add(new RowDefinition { Height = new GridLength(_smallheight) });
+            grid2.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+            grid2.RowDefinitions.Add(new RowDefinition { Height = new GridLength(_smallheight) });
+            //grid2.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_width) });
+
+            var imageButton = new ImageButton
+            {
+                Source = "asd",
+                BackgroundColor = Color.Pink
+            };
+            grid2.Children.Add (imageButton, 0, 0);
+
+            var image2 = new Image { Source = "sp02.jpg" };
+            var entry2 = new Label { Text = "test0002" };
+
+            grid2.Children.Add(image2, 0, 1);
+            grid2.Children.Add(entry2, 0, 2);
+
+            grid0.Children.Add (grid1, 0, 0);
+            grid0.Children.Add (grid2, 1, 0);
+            RootLayout.Children.Add (grid0);
+
+        }
+
+        private void AddItemLayout(Grid grid1, string v1, string v2, string v3, string v4)
+        {
+            //grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(130) });
+            //grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(60) });
+
+            ////triangle2.png
+
+            //var image1 = new Image { Source = v1 };
+            //var image2 = new Image { Source = "triangle2.png", VerticalOptions = LayoutOptions.End, Margin = new Thickness (30, 0, 0, 0) };
+            //var entry1 = new Label { Text = v2 };
+            //grid1.Children.Add(image1, 0, 0);
+            //grid1.Children.Add(image2, 0, 0);
+            //grid1.Children.Add(entry1, 0, 1);
+
         }
 
         async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
