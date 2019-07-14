68ecaf54f9df195cafba57cadb1a6afd147df0b2
Shaw6157 <43285754+Shaw6157@users.noreply.github.com>
2019-07-15 01:29:04 +1200

    [listing] update logic
    grid element method done.
    spacing fixed.
    
    todo:
    loop for the item list
    make a NuGet pkg

--- Demo0607/Demo0607/Demo0607/Views/ItemsPage.xaml.cs
+++ Demo0607/Demo0607/Demo0607/Views/ItemsPage.xaml.cs
@@ -28,44 +28,26 @@ namespace Demo0607.Views
             double _smallheight = 60;
             double _bigheight = 130;
 
-
             var grid0 = new Grid { ColumnSpacing = 0, HorizontalOptions = LayoutOptions.FillAndExpand };
             grid0.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
             grid0.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
 
+            var grid1 = new Grid
+            {
+                ColumnSpacing = 0,
+                RowSpacing = 0
+            };
+            var grid2 = new Grid
+            {
+                ColumnSpacing = 0,
+                RowSpacing = 0
+            };
 
-            var grid1 = new Grid { HorizontalOptions = LayoutOptions.CenterAndExpand };
-            grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(_bigheight) });
-            grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(_smallheight) });
-            grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(_bigheight) });
-            grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(_smallheight) });
+            AddItemLayout(grid1, "sp01.jpg", "test0001", "descsc", "", 0, 0);
+            AddItemLayout(grid1, "sp03.jpg", "test0003333", "dedededsc", "dedededsc", 2, 3);
 
-
-            var image1 = new Image { Source = "sp01.jpg" };
-            var entry1 = new Label { Text = "test0001" };
-            grid1.Children.Add(image1, 0, 0);
-            grid1.Children.Add(entry1, 0, 1);
-
-            //AddItemLayout(grid1, "sp01.jpg", "test0001", "", "");
-
-
-            var image3 = new Image { Source = "sp03.jpg" };
-            var entry3 = new Label { Text = "test0003" };
-            grid1.Children.Add(image3, 0, 2);
-            grid1.Children.Add(entry3, 0, 3);
-
-
-            var image99 = new Image { Source = "triangle2.png", HorizontalOptions = LayoutOptions.Start, VerticalOptions = LayoutOptions.End, Margin = new Thickness(30, 0, 0, 0), WidthRequest = 24, HeightRequest = 12 };
-            grid1.Children.Add(image99, 0, 0);
-            var image88 = new Image { Source = "triangle2.png", HorizontalOptions = LayoutOptions.Start, VerticalOptions = LayoutOptions.End, Margin = new Thickness(30, 0, 0, 0), WidthRequest = 24, HeightRequest = 12 };
-            grid1.Children.Add(image88, 0, 2);
-
-
-            var grid2 = new Grid { HorizontalOptions = LayoutOptions.CenterAndExpand };
             grid2.RowDefinitions.Add(new RowDefinition { Height = new GridLength(_smallheight) });
-            grid2.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
-            grid2.RowDefinitions.Add(new RowDefinition { Height = new GridLength(_smallheight) });
-            //grid2.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(_width) });
+            //grid2.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
 
             var imageButton = new ImageButton
             {
@@ -73,32 +55,54 @@ namespace Demo0607.Views
                 BackgroundColor = Color.Pink
             };
             grid2.Children.Add (imageButton, 0, 0);
-
-            var image2 = new Image { Source = "sp02.jpg" };
-            var entry2 = new Label { Text = "test0002" };
-
-            grid2.Children.Add(image2, 0, 1);
-            grid2.Children.Add(entry2, 0, 2);
-
+            
+            AddItemLayout(grid2, "sp02.jpg", "test002222", "qqqqqqc", "", 1, 1);
+            
             grid0.Children.Add (grid1, 0, 0);
             grid0.Children.Add (grid2, 1, 0);
-            RootLayout.Children.Add (grid0);
+            //RootLayout.Children.Add (grid0);
+            RootLayout.Content = grid0;
 
         }
 
-        private void AddItemLayout(Grid grid1, string v1, string v2, string v3, string v4)
+        private void AddItemLayout(Grid grid1, string v1, string v2, string v3, string v4, int index, int column)
         {
-            //grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(130) });
-            //grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(60) });
+            grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(130) });
+            //grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+            grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(30) });
+            grid1.RowDefinitions.Add(new RowDefinition { Height = new GridLength(30) });
 
-            ////triangle2.png
 
-            //var image1 = new Image { Source = v1 };
-            //var image2 = new Image { Source = "triangle2.png", VerticalOptions = LayoutOptions.End, Margin = new Thickness (30, 0, 0, 0) };
-            //var entry1 = new Label { Text = v2 };
-            //grid1.Children.Add(image1, 0, 0);
-            //grid1.Children.Add(image2, 0, 0);
-            //grid1.Children.Add(entry1, 0, 1);
+            var image1 = new Image { Source = v1, Aspect = Aspect.AspectFill };
+            var image2 = new Image { Source = "triangle2.png", HorizontalOptions = LayoutOptions.Start, VerticalOptions = LayoutOptions.End, Margin = new Thickness(30, 0, 0, 0), WidthRequest = 24, HeightRequest = 12 };
+            var entry1 = new Label { Text = v2, FontSize = 17, FontAttributes = FontAttributes.Bold, Margin = new Thickness(10, 8, 0, 0) };
+            var entry2 = new Label { Text = v3, FontSize = 13, Margin = new Thickness(10, 6, 0, 0) };
+            grid1.Children.Add(image1, 0, column);
+            grid1.Children.Add(image2, 0, column);
+            grid1.Children.Add(entry1, 0, column + 1);
+            grid1.Children.Add(entry2, 0, column + 2);
+
+            #region click grsture area
+            var clickable = new ContentView
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Opacity = 0.6
+            };
+
+            var tap = new TapGestureRecognizer();
+            tap.Tapped += async (object sender, EventArgs e) => {
+                var view = (ContentView)sender;
+                view.BackgroundColor = Color.Azure;
+                await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(viewModel.ToyListing[index])));
+                view.BackgroundColor = Color.Transparent;
+            };
+            clickable.GestureRecognizers.Add(tap);
+
+            grid1.Children.Add(clickable, 0, column);
+
+            Grid.SetRowSpan(clickable, 3);
+            #endregion
 
         }
 
@@ -111,7 +115,7 @@ namespace Demo0607.Views
             await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
 
             // Manually deselect item.
-            ItemsListView.SelectedItem = null;
+            //ItemsListView.SelectedItem = null;
         }
 
         async void AddItem_Clicked(object sender, EventArgs e)
